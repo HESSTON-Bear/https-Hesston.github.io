5c7c770f9ee1b1aee1e5d723858645258d80dfca
HESSTON-Bear <127667010+HESSTON-Bear@users.noreply.github.com>
2023-12-27 10:37:02 +0800

    Add files via upload

---
+++ JS/Shoujun Zhou_javascript.js
@@ -0,0 +1,6 @@
+// Add your JavaScript code here
+
+$(document).ready(function () {
+    // jQuery animation for the first advertisement
+    $('.advertisement1').animate({ /* Your animation properties here */ }, 1000);
+});
